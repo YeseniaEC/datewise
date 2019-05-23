3c3deb4d0dd725b1f2b5130eb4c1e5a020bb9521
Yesenia Cardenas <yesenia.cardenas@mic-software.net>
2019-05-23 16:48:14 -0700

    working on the photo section, can update image successfully to main, working on displaying it on the navbar

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -9,19 +9,23 @@ import { User } from '../_models/user';
 })
 export class UserService {
 
-  baserUrl = environment.apiUrl;
+  baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.baserUrl + 'users');
+    return this.http.get<User[]>(this.baseUrl + 'users');
   }
 
   getUser(id): Observable<User> {
-    return this.http.get<User>(this.baserUrl + 'users/' + id);
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
   }
 
   updateUser(id: number, user: User) {
-    return this.http.put(this.baserUrl + 'users/' + id, user);
+    return this.http.put(this.baseUrl + 'users/' + id, user);
+  }
+
+  setMainPhoto(userId: number, id: number) {
+    return this.http.post(this.baseUrl + `users/${userId}/photos/${id}/setMain`, {});
   }
 }
